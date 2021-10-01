ae38106af01af1d26328b39e543404ec181af3d6
JBerggren <jonatan.berggren@gmail.com>
2021-10-01 08:26:23 +0200

    Inital commit with working servo control

---
+++ Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using Iot.Device.ServoMotor;
+using System;
+using System.Device.Pwm.Drivers;
+
+namespace HalloweenSkull
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pwmChannel = new SoftwarePwmChannel(18, 50, 0, true);
+            var servo = new ServoMotor(pwmChannel, 180, 1000, 2000);
+            servo.Start();
+            Console.WriteLine("Ready!");
+            try
+            {
+                while (true)
+                {
+                    var angle = Int32.Parse(Console.ReadLine());
+                    servo.WriteAngle(angle);
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Quitting!");
+            }
+           
+            servo.Stop();
+            pwmChannel.Stop();
+            servo.Dispose();
+            pwmChannel.Dispose();
+        }
+    }
+}
