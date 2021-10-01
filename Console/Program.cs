a5141686f31041424847e4ebccef3fbb956068d9
JBerggren <jonatan.berggren@gmail.com>
2021-10-01 16:42:29 +0200

    Update

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Iot.Device.ServoMotor;
+using NetCoreAudio;
 using System;
+using System.Device.Gpio;
 using System.Device.Pwm.Drivers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HalloweenSkull
 {
@@ -8,26 +12,82 @@ namespace HalloweenSkull
     {
         static void Main(string[] args)
         {
+            GpioController controller = new GpioController();
+            controller.OpenPin(5, PinMode.Output);
+            controller.Write(5, PinValue.Low);
+            while (true)
+            {
+                Console.WriteLine("What would you like to do?\n1 {angle}- Control servo\n2 {on/off} - Toggle light\n3 {file} - Play sound\n4 - Laugh\nq - Quit");
+                var choice = Console.ReadLine().Split(new char[] { ' ' });
+                switch (choice[0])
+                {
+                    case "1":
+                        var angle = int.Parse(choice[1]);
+                        TurnServo(angle);
+                        break;
+                    case "2":
+                        var on = choice[1] == "on";
+                        ToggleLights(controller, on);
+                        break;
+                    case "3":
+                        var file = choice[1];
+                        PlaySound(file);
+                        break;
+                    case "4":
+                        PlaySound("laugh.mp3");
+                        var stopBlink = new ManualResetEventSlim();
+                        BlinkLights(controller, stopBlink);
+                        TurnServo(180);
+                        TurnServo(0);
+                        TurnServo(180);
+                        TurnServo(0);
+                        TurnServo(180);
+                        TurnServo(0);
+                        stopBlink.Set();
+                        break;
+                    case "q":
+                        return;
+                }
+            }
+        }
+
+        private static void BlinkLights(GpioController controller, ManualResetEventSlim resetEvent)
+        {
+            Task.Run(() =>
+            {
+                while (!resetEvent.IsSet)
+                {
+                    controller.Write(5, PinValue.High);
+                    Thread.Sleep(100);
+                    controller.Write(5, PinValue.Low);
+                    Thread.Sleep(100);
+                }
+                controller.Write(5, PinValue.Low);
+            });
+        }
+
+        private static void PlaySound(string file)
+        {
+            var player = new Player();
+            player.Play(file);
+        }
+
+        private static void TurnServo(int angle)
+        {
             var pwmChannel = new SoftwarePwmChannel(18, 50, 0, true);
             var servo = new ServoMotor(pwmChannel, 180, 1000, 2000);
             servo.Start();
-            Console.WriteLine("Ready!");
-            try
-            {
-                while (true)
-                {
-                    var angle = Int32.Parse(Console.ReadLine());
-                    servo.WriteAngle(angle);
-                }
-            }catch(Exception ex)
-            {
-                Console.WriteLine("Quitting!");
-            }
-           
+            servo.WriteAngle(angle);
+            Thread.Sleep(300);
             servo.Stop();
             pwmChannel.Stop();
             servo.Dispose();
             pwmChannel.Dispose();
+        }        
+
+        private static void ToggleLights(GpioController controller, bool on)
+        {
+            controller.Write(5, on ? PinValue.High : PinValue.Low);
         }
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+# HalloweenSkull   
+Project for creating a interactive skull for Halloween with a Raspberry Pi and .Net 5.   
+The idea is to use a plastic skull and embed red leds behind the eyes, a servo to control the jaw and a speaker to play spoky laughter on and hook it up to a Raspberry Pi.
+Either I will use a battery or have the skull pluged in to electricity. 
+
+## Current progress   
+I have the leds, servo and speaker "working". Now need to get a good connection between the jaw and the servo. 
+
+## Ideas for further development   
+* Create a webpage hosted on the Pi to remote control it with
+* Make it possible to enter text into webpage which will then be spoken by the skull
+* Add btn/touch areas on the back of the skull to manually start it
+
+## Misc notes   
+Set volume - sudo amixer cset numid=1 100%
